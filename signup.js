728a1111fe16b7dea43cff435a33fbd8ef0a9605
Admin@Arnis <ag67300@ubt-uni.net>
2023-12-06 17:47:51 +0100

    update

--- signup.js
+++ signup.js
@@ -5,3 +5,29 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 }); 
+
+    let emailRegex =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let passwordRegex = /^[A-Z][a-z]{3,8}$/;
+
+    function validateForm(){
+        let emailInput = document.getElementById('email');// value me marr vlera inpute
+        let emailError = document.getElementById('emailError');
+        let passwordInput = document.getElementById('password');
+        let passwordError = document.getElementById('passwordError');
+
+        emailError.innerText = '';
+        passwordError.innerText = '';
+
+        if(!emailRegex.test(emailInput.value)){
+            emailError.innerText = 'invalid email';
+            return;
+        }
+        if(!passwordRegex.test(passwordInput.value)){
+            passwordError.innerText = 'invalid password';
+            return;
+        }
+
+        alert('form submitted succesfully!');
+    }
+    
+
